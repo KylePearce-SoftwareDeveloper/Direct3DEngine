31dc6b45689c1af4adfa03ae3bdd5d8447671c83
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2020-09-01 16:36:41 +0100

    #4 - Window Class

--- Direct3D/src/cpp/WinMain.cpp
+++ Direct3D/src/cpp/WinMain.cpp
@@ -1,83 +1,15 @@
-#include <Windows.h>
-#include "../hpp/WindowsMessageMap.h"
+#include "../hpp/Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mm;
-	OutputDebugString(mm(msg, lParam, wParam).c_str());
-
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(13);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Respects Payed");
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Willy");
-		}
-		break;
-	case WM_CHAR:
-		{
-			static std::string title;
-			title.push_back((char)wParam);
-			SetWindowText(hWnd, title.c_str());
-		}
-		break;
-	case WM_LBUTTONDOWN:
-	{
-		const POINTS pt = MAKEPOINTS(lParam);
-		std::ostringstream oss;
-		oss << "(" << pt.x << "," << pt.y << ")";
-		SetWindowText(hWnd, oss.str().c_str());
-	}
-	break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow)
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
 {
-	const auto pClassName = "Direct3D";
-	//Register window class.
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
-	//Create window instance.
-	HWND hwnd = CreateWindowEx(
-		0, pClassName,
-		"Direct3D Window",
-		WS_CAPTION | WS_MAXIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
+	Window wnd1(800, 300, "Direct3D - 1");
+	Window wnd2(1000, 100, "Direct3D - 2");
 
-	);
-	//Show the fucking window.
-	ShowWindow(hwnd, SW_SHOW);
-
-	//Message.
 	MSG msg;
 	BOOL gResult;
 	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
@@ -85,12 +17,11 @@ int CALLBACK WinMain(
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	if (gResult == -1)
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
+
+	return msg.wParam;
 }
--- Direct3D/src/hpp/WindowsMessageMap.h
+++ Direct3D/src/hpp/WindowsMessageMap.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <unordered_map>
-#include <Windows.h>
+#include "../hpp/WinDef.h"
 #include <string>
 #include <sstream>
 #include <iomanip>
